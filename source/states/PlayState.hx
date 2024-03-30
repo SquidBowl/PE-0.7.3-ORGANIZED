d6605bcd570d99ad0b0930fa913e37b141ff6538
Rosa <97431838+SquidBowl@users.noreply.github.com>
2024-03-30 01:15:12 -0400

    what the everliving fuck was i thinking

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1930,12 +1930,7 @@ class PlayState extends MusicBeatState
 				gfSpeed = Math.round(flValue1);
 
 			case 'White Flash':
-				var whiteGraphic:FlxSprite = new FlxSprite(0, 0);
-				whiteGraphic.makeGraphic(3560, 2560, 0xFFFFFFFF);
-				add(whiteGraphic);
-					whiteGraphic.scrollFactor.set(0, 0);
-					whiteGraphic.alpha = 1;
-					FlxTween.tween(whiteGraphic, {alpha: 0}, flValue1,);
+				camGame.flash(0xffffffff, flValue1);
 
 			case 'Add Camera Zoom':
 				if(ClientPrefs.data.camZooms && FlxG.camera.zoom < 1.35) {
